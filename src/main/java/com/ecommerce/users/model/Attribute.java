69e746b7fbd10d13b86e6f072104f5562f484ebb
hnik <hnik1996@yahoo.com>
2020-04-14 15:12:52 +0430

    feat: create attribute class

--- src/main/java/com/ecommerce/users/model/Attribute.java
+++ src/main/java/com/ecommerce/users/model/Attribute.java
@@ -1,4 +1,22 @@
 package com.ecommerce.users.model;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "attributes")
+@Data
 public class Attribute {
+    @Id
+    private String id;
+    private String attr;
+    private String value;
 }
