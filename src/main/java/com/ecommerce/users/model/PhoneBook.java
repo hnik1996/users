7223637192daf3080114231f37b6044944d5e6be
hnik <hnik1996@yahoo.com>
2020-04-14 15:13:33 +0430

    feat: create phone book class

--- src/main/java/com/ecommerce/users/model/PhoneBook.java
+++ src/main/java/com/ecommerce/users/model/PhoneBook.java
@@ -1,4 +1,34 @@
 package com.ecommerce.users.model;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "phonebook")
+@Data
 public class PhoneBook {
+    @Id
+    private long id;
+    private char status;
+    private long userId;
+    private String name;
+    private String msisdn;
+    @Column(name = "created_ts")
+    private Date createdTs;
+    @Column(name = "updated_ts")
+    private Date updatedTs;
+    @Column(name = "creator_user_id")
+    private long creatorUserId;
+    @Column(name = "updated_user_id")
+    private long updatedUserId;
+
 }
