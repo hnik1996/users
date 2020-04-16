ae4350a7519c6e0754c4316f7aca6040e53259ff
hnik <hnik1996@yahoo.com>
2020-04-16 20:55:46 +0430

    feat: create user not found exception

---
+++ src/main/java/com/ecommerce/users/controller/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package com.ecommerce.users.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
